fad38db3374e55e3691c0e4657258218ea5d4bdb
Orlando Carnate <42432006+orlandocarnate@users.noreply.github.com>
2018-12-15 10:27:11 -0600

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,17 @@
 // game.js
+
+// Global Variables
+
+// Player Object
+
+    // Health Points Property
+
+    // Attack Power Property
+
+    // Counter Attack Method
+
+// Enemy Object
+
+    // Counter Attack Power Property?
+
+// Display Character Cards
