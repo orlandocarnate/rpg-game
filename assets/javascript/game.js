d7d49ab5b0e4f7130f4b5d28ab3adbc7660788fd
Orlando Carnate <orlando.carnate@gmail.com>
2018-12-15 21:44:37 -0600

    Create website skeleton.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,29 @@
 
 // Global Variables
 
+// character objects
+var harry = {
+    name: "Harry Potter",
+    healthPoints: 5,
+    attackPower: 5,
+    counterAttackPower: 5
+}
+
+var hermione = {
+    name: "Hermione Granger",
+    healthPoints: 5,
+    attackPower: 5,
+    counterAttackPower: 5
+}
+
+var ron = {
+    name: "Ron Weasley",
+    healthPoints: 5,
+    attackPower: 5,
+    counterAttackPower: 5
+}
+
+
 // Player Object
 
     // Health Points Property
@@ -18,6 +41,8 @@
     // render cards with style tags using jQuery
 
 // Game Interaction Functions
+
+// Select Character
 
 // ATTACK button
 
