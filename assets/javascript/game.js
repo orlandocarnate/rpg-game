a1020403324977a3705127c8c1dcad4edd614574
Orlando Carnate <orlando.carnate@gmail.com>
2018-12-16 19:17:47 -0600

    Having problems selecting first and second cards. Will split to two different selector functions.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,18 @@
 $(document).ready(function() {
 
     // Global Variables
+    var canPickCard = true;
+    var isFirstCharacter = true;
 
-    // Bootstrap MODAL string
+    var playerName;
+    var playerHP;
+    var playerAttack;
+    var playerCounter;
+
+    var enemyName;
+    var enemyHP;
+    var enemyAttack;
+    var enemyCounter;
 
     // Gets Link for Theme Song
     var audioElement = document.createElement("audio");
@@ -50,7 +60,7 @@ $(document).ready(function() {
             console.log(value.counterAttackPower);
     
                 
-            $("#chooseChar").append("<div class='card' id='card" + key + "' style='width:200px' data-toggle='modal' data-target='#exampleModal'>");
+            $("#chooseChar").append("<div class='card' id='card" + key + "' style='width:200px'>");
             $("#card"+key).attr("name", value.name);
             $("#card"+key).attr("healthpoints", value.healthPoints);
             $("#card"+key).attr("attackpower", value.attackPower);
@@ -92,34 +102,89 @@ $(document).ready(function() {
 
     // Game Interaction Functions
 
+    
+
+    
+
     // Select Character
+    function selectCharacter () {
+
+            // alert("Card Clicked");
+    
+            // Get ID
+            cardID = "#"+$(this).attr("id");
+            // alert(cardID);
+    
+            // Get health, attack, and counter values
+            if (isFirstCharacter) {
+                // alert("First Pick");
+                playerName = $(this).attr("name");
+                playerHP = $(this).attr("healthPoints");
+                playerAttack = $(this).attr("attackPower");
+                playerCounter = $(this).attr("counterAttackPower");
+
+                // Move card to Your Character Section
+                $(cardID).appendTo("#yourCharacter");
+
+                // move remaining cards to Characters Left section
+                $(".card").not(cardID).each(function (index) {
+                    $(this).appendTo("#charactersLeft");
+                });
+
+                isFirstCharacter = false;
+
+            } else {
+                enemyName = $(this).attr("name");
+                enemyHP = $(this).attr("healthPoints");
+                enemyAttack = $(this).attr("attackPower");
+                enemyCounter = $(this).attr("counterAttackPower");
+                canPickCard = false;
+
+                // Move card to Defender Section
+                $(cardID).appendTo("#defender");
+            
+            }
+            
+    
+            // create text for Modal pop
+            // var modalText = ' health: '+ cardHP + ', attack: ' + cardAttack + ', counter: ' + cardCounter;
+            // $(".modal-title").text(cardName);
+            // $(".modal-body").text(modalText);
+    
+
+    
+            
+
+
+    };
 
     // ATTACK button
-    $(".attack-button").on("click", function() {
-        audioElement.play();
-      });
+    function attackButton() {
+        $(".attack-button").on("click", function() {
+            audioElement.play();
+        });
 
-    // get info from Cards on click
+    };
+
+
+    // jQuery Event Listeners
+
+    // Click Card event
     $(".card").on("click", function () {
-        // alert("Card Clicked");
+        // You can only select card if first or second is not selected
+        if (canPickCard) {
+            selectCharacter();
+            // If this is the first card then select first
+            // if (isFirstCharacter) {
+            //     isFirstCharacter = false;
+            //     selectCharacter();
+            // } else {
+            //     selectCharacter();
+            // }
 
-        // Get ID
-        cardID = "#"+$(this).attr("id");
-        // alert(cardID);
 
-        // Get health, attack, and counter values
-        var cardName = $(this).attr("name");
-        var cardHP = $(this).attr("healthPoints");
-        var cardAttack = $(this).attr("attackPower");
-        var cardCounter = $(this).attr("counterAttackPower");
+        }
+    });
 
-        // create text for Modal pop
-        var modalText = ' health: '+ cardHP + ', attack: ' + cardAttack + ', counter: ' + cardCounter;
-        $(".modal-title").text(cardName);
-        $(".modal-body").text(modalText);
-
-        // Move card to Your Character Section
-        $(cardID).appendTo("#charactersLeft");
-    })
-
+    
 });
